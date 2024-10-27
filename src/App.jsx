e395d5e8c8365ad498821d03608d2d0bb8799e60
alirezahosseinzadehw <137781640+alirezahosseinzadehw@users.noreply.github.com>
2024-10-27 22:58:16 +0330

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import {
+  Provider,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/layout",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return (
+    <div>
+      <Provider>
+        <RouterProvider router={router} />
+      </Provider>
+    </div>
+  );
+}
---
+++ src/ui/Header.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <Header>
+      <Link to="/">fast React pizza Co.</Link>
+      <p>Jonas</p>
+    </Header>
+  );
+}
